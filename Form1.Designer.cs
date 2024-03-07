980214c5ea7b90f5190c90c887727747fa100ff0
siamoloro <137732906+siamoloro@users.noreply.github.com>
2024-03-07 20:51:28 +0200

    Hangman Game in C#
    Hangman Game developed using C# that runs on Windows platform.

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿/*
+Author: Osiame Moloro
+Date: 02 May 2023
+Purpose: Auto-Generated code created the form and the controls.
+*/
+namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblWelcom = new System.Windows.Forms.Label();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.wrongAttempts = new System.Windows.Forms.ListBox();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.Score = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(284, 139);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblWelcom
+            // 
+            this.lblWelcom.AutoSize = true;
+            this.lblWelcom.BackColor = System.Drawing.Color.GhostWhite;
+            this.lblWelcom.Font = new System.Drawing.Font("Script MT Bold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcom.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lblWelcom.Location = new System.Drawing.Point(317, 20);
+            this.lblWelcom.Name = "lblWelcom";
+            this.lblWelcom.Size = new System.Drawing.Size(171, 44);
+            this.lblWelcom.TabIndex = 1;
+            this.lblWelcom.Text = "Hangman";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(442, 139);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 2;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(334, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(139, 24);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // wrongAttempts
+            // 
+            this.wrongAttempts.BackColor = System.Drawing.Color.White;
+            this.wrongAttempts.FormattingEnabled = true;
+            this.wrongAttempts.Location = new System.Drawing.Point(418, 212);
+            this.wrongAttempts.Name = "wrongAttempts";
+            this.wrongAttempts.Size = new System.Drawing.Size(120, 95);
+            this.wrongAttempts.TabIndex = 4;
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Location = new System.Drawing.Point(572, 103);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(0, 13);
+            this.lblAttempts.TabIndex = 5;
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.Location = new System.Drawing.Point(380, 176);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(0, 13);
+            this.lblWinner.TabIndex = 6;
+            // 
+            // Score
+            // 
+            this.Score.FormattingEnabled = true;
+            this.Score.Location = new System.Drawing.Point(206, 212);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(120, 95);
+            this.Score.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Final Score";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(439, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Wrong Attempts";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.wrongAttempts);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.lblWelcom);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblWelcom;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox wrongAttempts;
+        private System.Windows.Forms.Label lblAttempts;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.ListBox Score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿/*
+Author: Osiame Moloro
+Date: 02 May 2023
+Purpose: Programmer source code
+ */ 
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+        // All the fields to be used.
+        string word;
+        int index;
+        string asterisk = "";
+        StringBuilder sbWord;
+        StringBuilder sbAsterisk;
+        // The attepmtsLeft is set to 10
+        int attemptsLeft = 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Sets KeyPreview to true to allow the form to receive all key events.
+            this.KeyPreview = true;
+            
+        }
+
+        // Letters function that will accept a key as parameter.
+        void Letters(char keyPress)
+        {
+            // Rreplaces the key pressed characters to upper case
+            string charKey = keyPress.ToString().ToUpper();
+            keyPress = Char.Parse(charKey);
+
+            // An exeption is handled
+            try
+            {
+                // Check whether the key pressed is contained in the word and returns a bool value.
+                bool check = word.Contains(keyPress);
+                if (check == true)
+                {
+                    // Initiallized a new StingBuilder using the word string.
+                    sbWord = new StringBuilder(word);
+                    // Iterates through the characters in the word and changes them to the words pressed.
+                    for (int loop = 0; loop < sbWord.Length; loop++)
+                    {
+                        if (sbWord[loop] == keyPress)
+                        {
+                            sbAsterisk[loop] = sbWord[loop];
+                        }
+                        this.textBox1.Text = sbAsterisk.ToString();
+                        this.textBox1.Show();
+                    }
+                }
+                else
+                {
+                    // decrements the attemptsLeft by 1 if the character pressed is not contained int the word.
+                    attemptsLeft--;
+                    // Adds the wrong charater to the wrongAttempts list box.
+                    wrongAttempts.Items.Add(keyPress.ToString());
+                }
+
+            }
+            // An exeption is catched if a user tried to press a key before starting the game.
+            catch (ArgumentNullException)
+            {
+                // A message box is dislayed to the form as an error message.
+                MessageBox.Show("Please start first", "Hangman", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        // An event handler for the key press event on the from.
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Checks whether the key pressed is a letter from A-Z and returns a bool value.
+            bool alphaChecker = e.KeyChar.ToString().All(Char.IsLetter);
+
+            // Checks the alphaChecker variable and invokes the Letters Method it is true.
+            if (alphaChecker == true)
+            {
+                // Invokes the previous function.
+                // Puts the character pressed in the pressedKey parameter.
+                Letters(e.KeyChar);
+            }
+
+            if (sbWord == null)
+            {
+                this.lblAttempts.Text = $"Attempts Left: {attemptsLeft}";
+            }
+            else
+            {
+                // Checks whether all the keys pressed equal to the characters in the word and performs specified functions.
+                if (sbAsterisk.ToString() == sbWord.ToString())
+                {
+                    lblWinner.Text = "Winner";
+                    Score.Items.Add($"{word} {attemptsLeft}");
+                    wrongAttempts.Items.Clear();
+                    this.KeyPreview = false;
+
+                }
+                else if (attemptsLeft > 0)
+                {
+                    lblAttempts.Text = $"Attempts Left: {attemptsLeft.ToString()}";
+                }
+                // Checks the attemtsLeft and if it equals to 0 it will peform specified functions.
+                else if (attemptsLeft == 0)
+                {
+                    lblAttempts.Text = $"Attempts Left: {attemptsLeft.ToString()}";
+                    lblWinner.Text = "Game Over";
+                    this.KeyPreview = false;
+                }
+            }
+
+        }
+
+        // The load event handler of the form.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // All the controls are hidden when the from is loaded.
+            this.textBox1.Hide();
+            this.wrongAttempts.Hide();
+            this.Score.Hide();
+            label1.Hide();
+            label2.Hide();
+            this.lblAttempts.Hide();
+        }
+
+        // The start button event hander that will do the specified actions when the user presses it.
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // The KeyPreview is set to true and all the controls that are needed to display information are shown.
+            this.KeyPreview = true;
+            this.wrongAttempts.Show();
+            this.lblAttempts.Show();
+            this.Score.Show();
+            this.label1.Show();
+            this.label2.Show();
+            
+            // All the words used in the game are stored in an ArrayList
+            ArrayList Words = new ArrayList();
+            {
+                Words.Add("Beautiful");
+                Words.Add("Doctor");
+                Words.Add("House");
+                Words.Add("Separate");
+                Words.Add("Movie");
+                Words.Add("Drive");
+                Words.Add("Country");
+                Words.Add("Mountain");
+                Words.Add("Sleep");
+                Words.Add("Queen");
+                Words.Add("School");
+                Words.Add("Pleasent");
+            }
+
+            lblWelcom.Hide();
+            // The attemptsLeft variable is initiallized to 10.
+            this.attemptsLeft = 10;
+            lblAttempts.Text = $"Attempts Left: {attemptsLeft.ToString()}";
+
+            // The random class is intantiated and given functionality as specified.
+            Random random = new Random();
+            index = random.Next(0, Words.Count - 1);
+            word = Words[index].ToString().ToUpper();
+
+            this.btnStart.Text = "New Game";
+            // The words are changed to astricks and passed to a string builder.
+            asterisk = new String('*', word.Length);
+            sbAsterisk = new StringBuilder(asterisk);
+
+            // Controls are cleared when the new game button is pressed.
+            this.lblWinner.Text = null;
+            this.Score.Items.Clear();
+            this.wrongAttempts.Items.Clear();
+            textBox1.Text = asterisk.ToString();
+            this.textBox1.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Displays a message box on the form when the quit button is pressed.
+            DialogResult result = MessageBox.Show("Are you sure you want to quit the game?", "Hangman", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            // The application will close if the user presses yes.
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+    }
+}
